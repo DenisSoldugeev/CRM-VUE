c2b14c51f9b04b03381b961dd4bd56511308070b
Denis <soldugeev.d@cronix.ms>
2020-02-27 18:58:14 +0300

    localizetion

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import dateFilter from '@/filter/date.filter'
 import currencyFilter from '@/filter/currency.filter'
+import localizeFilter from '@/filter/localize.filter'
 import tooltipDirective from '@/directives/tooltip.directive'
 import Paginate from 'vuejs-paginate'
 import messagePlugin from '@/utils/message.plugin'
@@ -22,6 +23,7 @@ Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.filter('localize', localizeFilter)
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
 Vue.component('Paginate', Paginate)
